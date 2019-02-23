d5afc55bc61822a7cd0d6bedbcb874eab7a04847
Valentin Belonogov <valentin-belonogov@yandex.ru>
2019-02-23 23:53:38 +0100

    render annulus(pipe's front face)

--- PipeTestTask/Pipe.c
+++ PipeTestTask/Pipe.c
@@ -1,3 +1,4 @@
+#define _USE_MATH_DEFINES
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,11 +10,44 @@ static void
 pipe(GLfloat internal_radius, GLfloat external_radius, GLfloat length,
 	GLint segments)
 {	
+	GLfloat r0, r1;
+	GLfloat angle;
+	GLint i;	
+
+	r0 = internal_radius;
+	r1 = external_radius;
+	glShadeModel(GL_FLAT);
+	glNormal3f(0.f, 0.f, 1.f);
+
+	// Pipe's front face
+	glBegin(GL_QUAD_STRIP);
+	for (i = 0; i <= segments; i++) {
+		angle = i * 2.f * (float)M_PI / segments;
+		glVertex3f((float)cos(angle) * r0, (float)sin(angle) * r0, length * 0.5f);
+		glVertex3f((float)cos(angle) * r1, (float)sin(angle) * r1, length * 0.5f);
+	}
+	glEnd();
 }
+
+static GLint pipe1;
 
 //Draw function
 static void draw(void)
 {
+	static GLfloat color[4] = { 0.8f, 0.1f, 0.1f, 1.f };
+	pipe1 = glGenLists(1);
+	glNewList(pipe1, GL_COMPILE);
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, color);
+	pipe(1, 2, 10, 500);
+	glEndList();
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
+
+	glPushMatrix();
+	glTranslatef(0.0, 0.0, 0.0);
+	glRotatef(0.0, 0.0, 0.0, 1.0);
+	glCallList(pipe1);
+	glPopMatrix();	
 }
 
 //Input keys handling
@@ -53,8 +87,8 @@ void setwindow(GLFWwindow* window, int width, int height)
 static void init(void)
 {	
 	//light position
-	static GLfloat pos[4] = { 0.f, 0.f, 10.f, 0.f };
-	glLightfv(GL_LIGHT0, GL_POSITION, pos);
+	static GLfloat position[4] = { 0.f, 0.f, 10.f, 0.f };
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
