bfb2d75bf77b0f7e4cb571dc97e26164c42f4f7d
Valentin Belonogov <valentin-belonogov@yandex.ru>
2019-02-23 21:46:22 +0100

    functions set

--- PipeTestTask/Pipe.c
+++ PipeTestTask/Pipe.c
@@ -1,3 +1,45 @@
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+static void
+pipe(GLfloat internal_radius, GLfloat external_radius, GLfloat length,
+	GLint segments)
+{
+}
+
+//Draw function
+static void draw(void)
+{
+}
+
+//Input keys handling
+void key_press(GLFWwindow* window, int k, int l, int action)
+{
+}
+
+//Mouse wheel handling
+int scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+}
+
+//Rotating with mouse
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+}
+
+//window
+void setwindow(GLFWwindow* window, int width, int height)
+{
+}
+
+static void init(void)
+{
+}
+
 int main(int argc, char *argv[])
 {
 }
